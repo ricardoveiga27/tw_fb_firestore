89ac6cd9645a8fd7402cc246791a4a043debe3e7
Ricardo Veiga <ricardo.veiga@outlook.com.br>
2021-02-02 11:09:15 -0300

    iniciando projeto

---
+++ ios/Pods/Target Support Files/Pods-tw_fb_firestore2-tw_fb_firestore2Tests/Pods-tw_fb_firestore2-tw_fb_firestore2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tw_fb_firestore2_tw_fb_firestore2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_tw_fb_firestore2_tw_fb_firestore2Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-tw_fb_firestore2/Pods-tw_fb_firestore2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tw_fb_firestore2 : NSObject
+@end
+@implementation PodsDummy_Pods_tw_fb_firestore2
+@end
